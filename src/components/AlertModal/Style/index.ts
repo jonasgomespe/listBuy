e42ec8814c3ba92d0deddde246e5cb317aa78a67
jgsilva.ext <jgsilva.ext@orizon.com.br>
2023-07-16 22:06:04 -0300

    Foi criado um alert que vai ser exibido quando o usuario adicionar um no produto na lista

---
+++ src/components/AlertModal/Style/index.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const style = StyleSheet.create({
+    container:{
+        flex:1,
+        width:'100%',
+        height:'100%',
+        backgroundColor:'rgba(0,0,0,0.6)',
+        justifyContent:'center',
+        zIndex:3
+    },
+    containerForm:{
+        backgroundColor:'#fff',
+        margin:15,
+        padding:20,
+        borderRadius:10
+    },
+})
